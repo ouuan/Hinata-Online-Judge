c069e47ce598befa1305167c5a2864e70fe923bc
ouuan <y___o___u@126.com>
2019-09-21 10:49:27 +0800

    :sparkles:(web) comments of comments are marked now

--- web/js/uoj.js
+++ web/js/uoj.js
@@ -1199,4 +1199,10 @@ $(function() {
 		text = text.replace(/&gt;/g, '>').replace(/&lt;/g, '<').replace(/&amp;/g, '&');
 		$(this)[0].innerHTML = marked(text);
 	});
+	$('.comtbox6').each(function() {
+		var text = $(this)[0].innerHTML;
+		text = text.replace(/&gt;/g, '>').replace(/&lt;/g, '<').replace(/&amp;/g, '&');
+		$(this)[0].innerHTML = marked(text);
+	});
+
 });
