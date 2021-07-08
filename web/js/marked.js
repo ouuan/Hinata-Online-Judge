27ea476e7a5904b248ff0d249ac922ceab7e1853
Yufan You <ouuansteve@gmail.com>
2021-07-08 23:44:05 +0800

    fix: add classes to tables generated by marked.js

--- web/js/marked.js
+++ web/js/marked.js
@@ -1010,7 +1010,7 @@ Renderer.prototype.paragraph = function(text) {
 Renderer.prototype.table = function(header, body) {
   if (body) body = '<tbody>' + body + '</tbody>';
 
-  return '<table>\n'
+  return '<table class="table table-bordered table-hover table-striped">\n'
     + '<thead>\n'
     + header
     + '</thead>\n'
